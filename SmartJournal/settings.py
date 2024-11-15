2600d2c997af89aa525dd226e5ca9de89f20e0ec
Mohsen-Rahimi8001 <mohsenrahimi8001@gmail.com>
2024-11-15 10:48:28 +0330

    stash bug fixed

--- SmartJournal/settings.py
+++ SmartJournal/settings.py
@@ -11,11 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
-<<<<<<< Updated upstream
 from dotenv import load_dotenv
-=======
 from datetime import timedelta
->>>>>>> Stashed changes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
