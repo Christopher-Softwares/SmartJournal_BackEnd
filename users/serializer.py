068ae9c3850181146c3a4d1e2d075268a66150bc
Mohsen-Rahimi8001 <mohsenrahimi8001@gmail.com>
2024-12-12 18:53:47 +0330

    add reset password

--- users/serializer.py
+++ users/serializer.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from users.models import CustomUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     photo = serializers.ImageField(required=False)
@@ -7,3 +10,25 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['id', 'photo', 'bio', 'username', 'first_name', 'last_name', 'created_at', 'gender', 'age', 'country', 'city']
         read_only_fields = ['id']
+        
+class PasswordResetSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True)
+    new_password_repeat = serializers.CharField(write_only=True)
+    
+    def validate(self, data):
+        user = self.context["request"].user
+        
+        if not user.check_password(data['old_password']):
+            raise serializers.ValidationError({"message": "old password is incorrect."})
+        
+        if data["new_password"] != data["new_password_repeat"]:
+            raise serializers.ValidationError({"message": "new_password and new_password_repeat don't match."})
+        
+        return data
+    
+    def save(self):      
+        user = self.context["request"].user
+        user.set_password(self.validated_data["new_password"])
+        user.save()
+        return user
