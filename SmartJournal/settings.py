1fe91c2aef104d5d09228f03813078c67a2de89a
Aidin Asl Zaeim <140266369+AidinZaeim@users.noreply.github.com>
2024-12-26 03:11:34 -0800

    Update settings.py

--- SmartJournal/settings.py
+++ SmartJournal/settings.py
@@ -201,7 +201,7 @@ SPECTACULAR_SETTING = {
 	}
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=90),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
