a8de26abcc91a0f45c1ee20406e29d6a08aa0850
Mohsen-Rahimi8001 <mohsenrahimi8001@gmail.com>
2025-01-27 17:09:42 +0330

    add tag tests

--- note/tests.py
+++ note/tests.py
@@ -295,3 +295,205 @@ class GetNoteContentTests(APITestCase):
         
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         
+
+
+class TagEndpointsTests(APITestCase):
+    
+    def setUp(self):
+
+        self.client = APIClient()
+
+        self.plan = Plan.objects.create(
+            name="free plan", 
+            price=0, 
+            ai_access=True, 
+            duration=dt.timedelta(days=100), 
+            max_workspaces_count=20, 
+            max_notes_count=100, 
+            max_collaborator_count=100
+        )
+
+        self.user = CustomUser.objects.create_user(email="testuser@test.com", password="aA!12345678")
+        self.user2 = CustomUser.objects.create_user(email="testuser2@test.com", password="aA!123456789")
+        
+        self.workspace = Workspace.objects.create(
+            name="Test Workspace",
+            description="A workspace for testing",
+            owner=self.user,
+        )
+
+        self.workspace2 = Workspace.objects.create(
+            name="Test Workspace",
+            description="A workspace for testing",
+            owner=self.user2,
+        )
+        
+        self.tag1 = Tag.objects.create(
+            workspace=self.workspace, description="test desc", name="starred",
+        )
+
+        self.tag2 = Tag.objects.create(
+            workspace=self.workspace2, description="test desc2", name="starred2" 
+        )
+        
+        self.note1 = Note.objects.create(
+            workspace=self.workspace, title="note1", description="desc1",
+        )
+        
+        self.note2 = Note.objects.create(
+            workspace=self.workspace2, title="note2", description="desc2",
+        )
+
+
+    def test_create_tag_valid(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse("create-tag")
+        
+        valid_data = {
+            "workspace_id": self.workspace.id,
+            "name": "important",
+            "description": "test"
+        }
+        
+        response = self.client.post(url, valid_data)
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+    def test_create_tag_invalid(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse("create-tag")
+
+        invalid_data = {
+            "workspace_id": self.workspace2.id,
+            "name": "important2",
+            "description": "hello",
+        }
+        
+        response = self.client.post(url, invalid_data)
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+        
+    def test_delete_tag_valid(self):
+        
+        self.client.force_authenticate(user=self.user)
+        valid_url = reverse("delete-tag", kwargs={"pk": self.tag1.id})
+        
+        response = self.client.delete(valid_url)
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        
+
+    def test_delete_tag_not_found(self):
+        self.client.force_authenticate(user=self.user)
+        not_found_url = reverse("delete-tag", kwargs={"pk": self.tag1.id + 1000})
+        
+        response = self.client.delete(not_found_url)
+        
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        
+    
+    def test_delete_tag_forbidden(self):
+        self.client.force_authenticate(user=self.user)
+        forbidden_url = reverse("delete-tag", kwargs={"pk": self.tag2.id})
+        
+        response = self.client.delete(forbidden_url)
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    
+    def test_put_tag_valid(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse("update-tag")
+        
+        valid_data = {
+            "id": self.tag1.id,
+            "name": "new name",
+            "description": "new description",
+        }
+        
+        response = self.client.put(url, valid_data)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    
+    def test_put_tag_forbidden(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse("update-tag")
+        
+        forbidden_data = {
+            "id": self.tag2.id,
+            "name": "new name",
+            "description": "new desc",
+        }
+        
+        response = self.client.put(url, forbidden_data)
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+    def test_attach_valid(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse("attach-note-to-tag")
+        
+        valid_data = {
+            "tag_id": self.tag1.id,
+            "note_id": self.note1.id,
+        }
+        
+        response = self.client.post(url, valid_data)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_attach_forbidden(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse("attach-note-to-tag")
+
+        forbidden_data1 = {
+            "tag_id": self.tag1.id,
+            "note_id": self.note2.id,
+        }
+        
+        forbidden_data2 = {
+            "tag_id": self.tag2.id,
+            "note_id": self.note1.id,
+        }
+        
+        response1 = self.client.post(url, forbidden_data1)
+        response2 = self.client.post(url, forbidden_data2)
+        
+        self.assertEqual(response1.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response2.status_code, status.HTTP_403_FORBIDDEN)
+
+
+    def test_detach_valid(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse("detach-note-from-tag")
+
+        self.tag1.notes.add(self.note1)
+
+        valid_data = {
+            "note_id": self.note1.id,
+            "tag_id": self.tag1.id,
+        }
+        
+        response = self.client.post(url, valid_data)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    
+    def test_detach_forbidden(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse("detach-note-from-tag")
+
+        forbidden_data = {
+            "note_id": self.note2.id,
+            "tag_id": self.tag1.id,
+        }
+        
+        response = self.client.post(url, forbidden_data)
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
